c5922b0ac60fa890ed90562cb5bcba41f1353a54
AlexsanderBorges <aemgbo@gmail.com>
2020-04-21 22:59:24 -0300

    change  address displacement handler

--- Instructions/InstructionFormat.py
+++ Instructions/InstructionFormat.py
@@ -8,6 +8,8 @@ import struct
 
 Operandssize = {'b' : x86_Operand_size.Operands8.value , 'w' : x86_Operand_size.Operands16.value , 'l' : x86_Operand_size.Operands32.value}
 
+Dispvalue    = {'b' : x86_Mod_options.OneByteDisplacement.value , 'w' : x86_Mod_options.FourByteDisplacement.value , 'l' : x86_Mod_options.FourByteDisplacement.value}
+
 Immediatereg = {'add' : 0  , 'sub' : 5}
 
 def prefix(suffix):
@@ -35,13 +37,17 @@ def make2opcode(mnemonic , suffix , source_type  ,  destination_type):
       elif isinstance(source_type  , Number) and isinstance(destination_type , Register): 
           DirectionBit = 0
           opcode = instruction_table2OPImme[mnemonic] | DirectionBit << 1 | determineopsize(suffix)  
-      
+      elif isinstance(source_type  , Address) and isinstance(destination_type , Register): 
+          DirectionBit = 1
+          opcode = instruction_table2OP[mnemonic] | DirectionBit << 1 | determineopsize(suffix)         
+    
+
       by = bytearray()
       by.append(opcode)
-      
+      debug(by)
       return by
 
-def makeImmediate(source , suffix): 
+def makeImmediateorDisp(source , suffix): 
       if suffix == 'b': 
           return  struct.pack('<b' , source.Get_Number())
       if suffix == 'w': 
@@ -49,20 +55,41 @@ def makeImmediate(source , suffix):
       if suffix == 'l': 
           return  struct.pack('<i' , source.Get_Number())
 
+def MakeDisp(source , suffix): 
+      if suffix == 'b': 
+          return  struct.pack('<b' , source.Get_Disp())
+      if suffix == 'w': 
+          return  struct.pack('<h' , source.Get_Disp())
+      if suffix == 'l': 
+          return  struct.pack('<i' , source.Get_Disp())
+
+
 def makemod_rm2(mnemonic , source , destination , suffix):
       if isinstance(source  , Register) and isinstance(destination, Register): 
         modrm = bytearray()
         modrm.append(x86_Mod_options.RegisterAddress.value << 6 |  source.Get_RegisterNumber()  << 3 | destination.Get_RegisterNumber())
         return  modrm
+      
       if isinstance(source  , Number) and isinstance(destination, Register): 
 
          reg    = Immediatereg[mnemonic]
-       
          modrm = bytearray()
-      
          modrm.append(x86_Mod_options.RegisterAddress.value << 6 |  reg  << 3 | destination.Get_RegisterNumber())
-         
          return modrm + makeImmediate(source , suffix)
+
+      if isinstance(source , Address) and  isinstance(destination , Register): 
+         modrm = bytearray()
+         if  source.Get_Disp() == None: 
+            modrm.append(x86_Mod_options.RegisterIndirect.value << 6 |  destination.Get_RegisterNumber()  << 3 | source.Get_base())
+            return modrm
+         elif source.Get_Disp() != None: 
+            modfield = Dispvalue.get(suffix)
+            modrm.append(modfield << 6 |  destination.Get_RegisterNumber()  << 3 | source.Get_base())
+            print( destination.Get_RegisterNumber() )
+            return modrm + MakeDisp(source , suffix)
+
+
+    
 
 
 
